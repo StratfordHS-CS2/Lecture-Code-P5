8d3b775a1b49ef778ecc839f05676603eb22e87d
Dave Avis <charles.avis@springbranchisd.com>
2018-09-07 13:21:57 -0500

    Added Methods lecture

---
+++ 04 - MethodsPractice/Cat.java
@@ -0,0 +1,56 @@
+public class Cat
+{
+    /**
+     * Prints the string "Meow!" every time it is called.
+     * void means there is no information returned to the 
+     * calling method.
+     */
+    public static void speak()
+    {
+        System.out.println("Meow!");
+    }
+    
+    /**
+     * Returns the string "Fluffy" since that is the cat's name.
+     * String in the first line means that a String value must
+     * be returned. There must be a return statement. Whenever
+     * there is a return statement it must be documented in the
+     * javadoc comment.
+     * 
+     * @return  The name of the cat. 
+     */
+    public static String name()
+    {
+        String myName = "Fluffy";
+        return myName;
+    }
+    /**
+     * The cat will sleep for a specified number of minutes. Anytime you
+     * have a parameter sent to the method you must add a @param tag to 
+     * the javadoc.
+     * 
+     * @param   minutes     How many minutes the cat should nap.
+     */
+    public static void takeANap( int minutes )
+    {
+        int time = minutes;
+        System.out.println( "Zzzz... Sleeping for " + time + " minutes." );
+    }
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello. I am a cat." );
+        System.out.print( "What does the cat say? " );
+        speak();
+        System.out.println( "My name is " + name() );
+        name();   // does nothing because it returns a string, doesn't print a string.
+        System.out.println( "Take a 5 minute nap." );
+        takeANap(5);
+    }
+}
+
+
+
+
+
+
